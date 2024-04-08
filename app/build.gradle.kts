0b04641eba9f09c37c8bc0de026e1934811ba514
Abhishek <abhishekmajhi42@gmail.com>
2024-04-08 06:41:36 +0530

    -fixed issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,9 +63,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2024.04.00"))
-    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui:1.2.0")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.2.0")
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:2024.04.00"))
 
